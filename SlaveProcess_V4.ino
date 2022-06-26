20b2038c5aaf54080737fe3add0dca7ba2fe6a9e
Miguel Orozco <39131275+Necrovalle@users.noreply.github.com>
2022-06-25 18:46:43 -0600

    Update SlaveProcess_V4.ino
    version 9 para cambio a comunicacion unidireccional

--- SlaveProcess_V4.ino
+++ SlaveProcess_V4.ino
@@ -1,19 +1,21 @@
 
 // https://www.youtube.com/watch?v=ZR9QEWpGEDo
-
+/*V4 Modificacion Miguel
+  V6 Time Out
+  V10 comunicacion unidireccional
+*/
 #define LED       13
 #define SLAVE_EN_TX  2  //D3 Morado (DE)
 #define SLAVE_EN_RX  3  //D3 Verde  (RE)
-
 #define Relay1    4
 #define Relay2    5
 #define Relay3    6
 #define Relay4    7
 #define Relay5    8
 char var, var2;
-char ID='1';
+char ID = '6';
 int i;
-
+double Timer = 0;
 
 void setup() {
   pinMode(LED , OUTPUT);                        // Declare LED pin as output
@@ -27,81 +29,151 @@ void setup() {
   Serial.begin(9600);                           // set serial communication baudrate
   digitalWrite(SLAVE_EN_TX , LOW);
   digitalWrite(SLAVE_EN_RX , LOW);                 // Make Enable pin low
-  digitalWrite(Relay1 , LOW);
-  digitalWrite(Relay2 , LOW);
-  digitalWrite(Relay3 , LOW);
-  digitalWrite(Relay4 , LOW);
-  digitalWrite(Relay5 , LOW);
+  digitalWrite(Relay1 , HIGH);
+  digitalWrite(Relay2 , HIGH);
+  digitalWrite(Relay3 , HIGH);
+  digitalWrite(Relay4 , HIGH);
+  digitalWrite(Relay5 , HIGH);
   // Receiving mode ON
   i = 0;
 }
 
 void loop() {
+
   while (Serial.available())                    // If serial data is available then enter into while loop
   {
     var = Serial.read();                  // if available data is A
     digitalWrite(LED, HIGH);
+
     if (var == ID) {
 
+      Timer = millis();
+
       //Cambiar a escribir
-      
+
       digitalWrite(SLAVE_EN_TX , HIGH); digitalWrite(SLAVE_EN_RX , HIGH);    // Make Enable pin high to send Data
-      
-      delay(50);Serial.print(ID);Serial.flush();  
-      
+      delay(10);
+      // delay(50); Serial.print(";"); Serial.print(ID); Serial.flush();
+      Serial.print(ID); Serial.flush();
       // set serial communication  READ
       digitalWrite(SLAVE_EN_RX , LOW); digitalWrite(SLAVE_EN_TX , LOW);                // Make Enable pin low
-      delay(50);
-      i = 0;
+      delay(5);
+      i = 0; //Bandera
 
       while (i == 0) {
 
         while (Serial.available())                    // If serial data is available then enter into while loop
         {
           var2 = Serial.read();                  // if available data is A
-          digitalWrite(SLAVE_EN_RX , HIGH); digitalWrite(SLAVE_EN_TX , HIGH);
-          delay(20);
-          //Serial.print(var2);
+          //          digitalWrite(SLAVE_EN_RX , HIGH); digitalWrite(SLAVE_EN_TX , HIGH);
+                    delay(10);
+          //          Serial.print(":"); Serial.print(var2);
           switch (var2) {
-            case 'A':  
-              Serial.print("A"); Serial.flush(); delay(300);//  
-              digitalWrite(Relay1 , LOW); delay(20); //
-              digitalWrite(Relay2 , LOW); delay(20);
-              digitalWrite(Relay3 , LOW); delay(20);
-              digitalWrite(Relay4 , LOW); delay(20);
-              digitalWrite(Relay5 , LOW); delay(20);
-              break;
-            case 'B':
-              Serial.print("B"); Serial.flush(); delay(300);//  
+            case 'A':
+              Serial.print("A"); Serial.flush(); delay(300);//Operacion
               digitalWrite(Relay1 , HIGH); delay(20); //
               digitalWrite(Relay2 , HIGH); delay(20);
               digitalWrite(Relay3 , HIGH); delay(20);
               digitalWrite(Relay4 , HIGH); delay(20);
-              digitalWrite(Relay5 , LOW);  delay(20);
-              break;
-            case 'C':
-              Serial.print("C"); Serial.flush(); delay(300);//  
-              digitalWrite(Relay1 , LOW ); delay(20); //
-              digitalWrite(Relay2 , HIGH); delay(20);
-              digitalWrite(Relay3 , LOW ); delay(20);
-              digitalWrite(Relay4 , LOW ); delay(20);
               digitalWrite(Relay5 , HIGH); delay(20);
               break;
-            default:
-              Serial.print("Zero"); Serial.flush(); delay(300);//  
+            case 'B':
+              Serial.print("B"); Serial.flush(); delay(300);//Retorolavado
               digitalWrite(Relay1 , LOW); delay(20); //
               digitalWrite(Relay2 , LOW); delay(20);
               digitalWrite(Relay3 , LOW); delay(20);
               digitalWrite(Relay4 , LOW); delay(20);
+              digitalWrite(Relay5 , HIGH);  delay(20);
+              break;
+            case 'C':
+              Serial.print("C"); Serial.flush(); delay(300);//Enjuague
+              digitalWrite(Relay1 ,HIGH  ); delay(20); //
+              digitalWrite(Relay2 , LOW); delay(20);
+              digitalWrite(Relay3 , HIGH ); delay(20);
+              digitalWrite(Relay4 , HIGH ); delay(20);
               digitalWrite(Relay5 , LOW); delay(20);
+              break;
+
+            case 'H':
+              Serial.print("H"); Serial.flush(); delay(300);//Valvula 1
+              digitalWrite(Relay1 , HIGH  ); delay(20); //
+              break;
+
+            case 'h':
+              Serial.print("h"); Serial.flush(); delay(300);//
+              digitalWrite(Relay1 , LOW ); delay(20); //
+              break;
+
+            case 'I':
+              Serial.print("I"); Serial.flush(); delay(300);//Valvula 2
+              digitalWrite(Relay2 , HIGH ); delay(20); //
+              break;
+
+            case 'i':
+              Serial.print("i"); Serial.flush(); delay(300);//
+              digitalWrite(Relay2 , LOW ); delay(20); //
+              break;
+
+            case 'J':
+              Serial.print("J"); Serial.flush(); delay(300);//Valvula 3
+              digitalWrite(Relay3 , LOW ); delay(20); //
+              break;
+
+            case 'j':
+              Serial.print("j"); Serial.flush(); delay(300);//
+              digitalWrite(Relay3 , HIGH ); delay(20); //
+              break;
+
+            case 'K':
+              Serial.print("K"); Serial.flush(); delay(300);//Valvula 4
+              digitalWrite(Relay4 , LOW ); delay(20); //
+              break;
+
+            case 'k':
+              Serial.print("k"); Serial.flush(); delay(300);//
+              digitalWrite(Relay4 , HIGH ); delay(20); //
+              break;
+
+            case 'L':
+              Serial.print("L"); Serial.flush(); delay(300);//Valvula 5
+              digitalWrite(Relay5 , LOW ); delay(20); //
+              break;
+
+            case 'l':
+              Serial.print("l"); Serial.flush(); delay(300);//
+              digitalWrite(Relay5 , HIGH ); delay(20); //
+              break;
+
+            case 'X':
+              Serial.print("X"); Serial.flush(); delay(300);// Todas Cerradas
+              digitalWrite(Relay1 , LOW); delay(20); //
+              digitalWrite(Relay2 , LOW); delay(20);
+              digitalWrite(Relay3 , HIGH); delay(20);
+              digitalWrite(Relay4 , HIGH); delay(20);
+              digitalWrite(Relay5 , HIGH); delay(20);
+              break;
+
+            default:
+              Serial.print("Z"); Serial.flush(); delay(300);// Todas Cerradas
+
               break;
           }
           i = 1;
           digitalWrite(LED, LOW);
           var = "0";
-          digitalWrite(SLAVE_EN_RX , LOW); digitalWrite(SLAVE_EN_TX , LOW); 
+          digitalWrite(SLAVE_EN_RX , LOW); digitalWrite(SLAVE_EN_TX , LOW);
         }
+
+        if (((millis() - Timer) > 2000) && (i == 0))
+        {
+          i = 1;
+          Serial.println('N');
+        }
+
+
       }
+
+
     }
   }
 }
